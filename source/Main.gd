5ec1f181a620d653ab3377626cbee10991f146ff
Siphesihle Mhlongo <sihlecodes@gmail.com>
2023-08-11 15:37:22 +0200

    QOL: improved focus handling

--- source/Main.gd
+++ source/Main.gd
@@ -1,16 +1,30 @@
 extends Node
 
+func focus_message_edit():
+	if not %message_edit.focus_mode:
+		%message_edit.focus_mode = Control.FOCUS_ALL
+
+	%message_edit.grab_focus()
+
+func _ready() -> void:
+	if $Networking.username:
+		get_viewport().focus_entered.connect(focus_message_edit)
+
 func _on_send_pressed() -> void:
 	%message_edit.text_submitted.emit(%message_edit.text)
-	%message_edit.grab_focus()
+	focus_message_edit()
 
 func _on_message_edit_text_submitted(message: String) -> void:
 	$Networking.serve_message(message)
 	%message_edit.clear()
 
-func _on_messages_focus_entered() -> void:
-	%message_edit.grab_focus()
-
 func _on_username_prompt_submit(username: String) -> void:
 	$Networking.username = username
 	$Networking.create_or_join()
+	focus_message_edit()
+
+func _on_messages_focus_entered() -> void:
+	if $Networking.username:
+		focus_message_edit()
+	else:
+		%username.grab_focus()
